48198800a29ad8f3f014ce9efdd15bfa40bb45c3
Joe Holden <joemattholden@gmail.com>
2023-04-09 18:53:41 -0500

    Add files via upload

--- AFM_violin_plots.py
+++ AFM_violin_plots.py
@@ -1,23 +1,54 @@
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly
-import plotly.express as px
-
-import plotly.graph_objects as go
-
 import pandas as pd
+import plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from itertools import cycle
 
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/violin_data.csv")
+df = pd.read_excel("test_sns.xlsx")
+groups = ['G1', 'G2', 'G3']
 
-fig = go.Figure()
+def make_plots(dataframe, title, show=True):
+    """
+    :param dataframe takes in a dataframe where each column has Young's Modulus Data for a single run
+            This is a truncated dataframe, only containing data that should be plotted
+    Shows plotly violin plot
+    saves PNG and HTML
+    """
 
-days = ['Thur', 'Fri', 'Sat', 'Sun']
+    plot_title = title
+    fig = go.Figure()
 
-for day in days:
-    fig.add_trace(go.Violin(x=df['day'][df['day'] == day],
-                            y=df['total_bill'][df['day'] == day],
-                            name=day,
-                            box_visible=True,
-                            meanline_visible=True))
+    palette = cycle(px.colors.sequential.Plasma)
+    print(palette)
 
-fig.show()
+    for g in groups:
+        fig.add_trace(go.Violin(x=[g] * len(df[g]),
+                                y=df[g],
+                                name=g,
+                                box_visible=True,
+                                meanline_visible=True,
+                                marker_color=next(palette
+                                                  )))
+        next(palette)
+        next(palette)
+        next(palette)
+        next(palette)
+        next(palette)
+
+    fig.update_layout(
+        font=dict(
+            family="Arial",
+            size=60,  # Set the font size here
+            color="Black"
+        ),
+        title_text=plot_title,
+        title_x=0.5,
+        title_y=0.99
+    )
+
+    if show:
+        fig.show()
+    plotly.offline.plot(fig, filename=f'{title}.html')
+
+
+make_plots(df, "Sample 6 Spot 5")
